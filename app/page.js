3164451ad36e70331a501d4a645fd144b6e6e536
LipsaDas0710 <lipsadas0710@gmail.com>
2025-11-23 23:51:41 +0530

    Add landing page UI

--- app/page.js
+++ app/page.js
@@ -10,7 +10,7 @@ import Footer from "@/components/ui/Footer";
 
 export default function Home() {
   return (
-    <main className="bg-gradient-to-t from-slate-950 via-blue-850 to-gray-900">
+    <main className="bg-linear-to-t from-slate-950 via-blue-850 to-gray-900">
       <FloatingNav navItems={navItems} />
       <Hero />
       <Grid />
