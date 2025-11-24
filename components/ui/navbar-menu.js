197b109b93f942668ac0cba17a552a4359a001d6
LipsaDas0710 <lipsadas0710@gmail.com>
2025-11-24 21:12:15 +0530

    Add landing page UI

--- components/ui/navbar-menu.js
+++ components/ui/navbar-menu.js
@@ -1,206 +1,108 @@
-// "use client";
-// import React from "react";
-// import { motion } from "motion/react";
-
-
-
-// const transition = {
-//   type: "spring",
-//   mass: 0.5,
-//   damping: 11.5,
-//   stiffness: 100,
-//   restDelta: 0.001,
-//   restSpeed: 0.001,
-// };
-
-// export const MenuItem = ({
-//   setActive,
-//   active,
-//   item,
-//   children
-// }) => {
-//   return (
-//     <div onMouseEnter={() => setActive(item)} className="relative ">
-//       <motion.p
-//         transition={{ duration: 0.3 }}
-//         className="cursor-pointer text-black hover:opacity-[0.9] dark:text-white">
-//         {item}
-//       </motion.p>
-//       {active !== null && (
-//         <motion.div
-//           initial={{ opacity: 0, scale: 0.85, y: 10 }}
-//           animate={{ opacity: 1, scale: 1, y: 0 }}
-//           transition={transition}>
-//           {active === item && (
-//             <div
-//               className="absolute top-[calc(100%_+_1.2rem)] left-1/2 transform -translate-x-1/2 pt-4">
-//               <motion.div
-//                 transition={transition}
-//                 // layoutId ensures smooth animation
-//                 layoutId="active"
-//                 className="bg-white dark:bg-black backdrop-blur-sm rounded-2xl overflow-hidden border border-black/[0.2] dark:border-white/[0.2] shadow-xl">
-//                 <motion.div
-//                   // layout ensures smooth animation
-//                   layout
-//                   className="w-max h-full p-4">
-//                   {children}
-//                 </motion.div>
-//               </motion.div>
-//             </div>
-//           )}
-//         </motion.div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export const Menu = ({
-//   setActive,
-//   children
-// }) => {
-//   return (
-//     <nav
-//       // resets the state
-//       onMouseLeave={() => setActive(null)}
-//       className="relative rounded-full border border-transparent dark:bg-black dark:border-white/[0.2] bg-white shadow-input flex justify-center space-x-4 px-8 py-6 ">
-//       {children}
-//     </nav>
-//   );
-// };
-
-// export const ProductItem = ({
-//   title,
-//   description,
-//   href,
-//   src
-// }) => {
-//   return (
-//     <a href={href} className="flex space-x-2">
-//       <img
-//         src={src}
-//         width={140}
-//         height={70}
-//         alt={title}
-//         className="shrink-0 rounded-md shadow-2xl" />
-//       <div>
-//         <h4 className="text-xl font-bold mb-1 text-black dark:text-white">
-//           {title}
-//         </h4>
-//         <p className="text-neutral-700 text-sm max-w-[10rem] dark:text-neutral-300">
-//           {description}
-//         </p>
-//       </div>
-//     </a>
-//   );
-// };
-
-// export const HoveredLink = ({
-//   children,
-//   ...rest
-// }) => {
-//   return (
-//     <a
-//       {...rest}
-//       className="text-neutral-700 dark:text-neutral-200 hover:text-black ">
-//       {children}
-//     </a>
-//   );
-// };
-
-
 "use client";
-import React, { useState } from "react";
-import {
-  motion,
-  AnimatePresence,
-  useScroll,
-  useMotionValueEvent,
-} from "framer-motion";
-import Link from "next/link";
-import { cn } from "@/lib/utils";
+import React from "react";
+import { motion } from "motion/react";
 
-export const FloatingNav = ({
-  navItems,
-  className,
-}: {
-  navItems: {
-    name: string;
-    link: string;
-    icon?: JSX.Element;
-  }[];
-  className?: string;
+
+
+const transition = {
+  type: "spring",
+  mass: 0.5,
+  damping: 11.5,
+  stiffness: 100,
+  restDelta: 0.001,
+  restSpeed: 0.001,
+};
+
+export const MenuItem = ({
+  setActive,
+  active,
+  item,
+  children
 }) => {
-  const { scrollYProgress } = useScroll();
-
-  // set true for the initial state so that nav bar is visible in the hero section
-  const [visible, setVisible] = useState(true);
-
-  useMotionValueEvent(scrollYProgress, "change", (current) => {
-    // Check if current is not undefined and is a number
-    if (typeof current === "number") {
-      let direction = current! - scrollYProgress.getPrevious()!;
-
-      if (scrollYProgress.get() < 0.05) {
-        // also set true for the initial state
-        setVisible(true);
-      } else {
-        if (direction < 0) {
-          setVisible(true);
-        } else {
-          setVisible(false);
-        }
-      }
-    }
-  });
-
   return (
-    <AnimatePresence mode="wait">
-      <motion.div
-        initial={{
-          opacity: 1,
-          y: -100,
-        }}
-        animate={{
-          y: visible ? 0 : -100,
-          opacity: visible ? 1 : 0,
-        }}
-        transition={{
-          duration: 0.2,
-        }}
-        className={cn(
-          // change rounded-full to rounded-lg
-          // remove dark:border-white/[0.2] dark:bg-black bg-white border-transparent
-          // change  pr-2 pl-8 py-2 to px-10 py-5
-          "flex max-w-fit md:min-w-[70vw] lg:min-w-fit fixed z-[5000] top-10 inset-x-0 mx-auto px-10 py-5 rounded-lg border border-black/.1 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] items-center justify-center space-x-4",
-          className
-        )}
-        style={{
-          backdropFilter: "blur(16px) saturate(180%)",
-          backgroundColor: "rgba(17, 25, 40, 0.75)",
-          borderRadius: "12px",
-          border: "1px solid rgba(255, 255, 255, 0.125)",
-        }}
-      >
-        {navItems.map((navItem: any, idx: number) => (
-          <Link
-            key={`link=${idx}`}
-            href={navItem.link}
-            className={cn(
-              "relative dark:text-neutral-50 items-center  flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
-            )}
-          >
-            <span className="block sm:hidden">{navItem.icon}</span>
-            {/* add !cursor-pointer */}
-            {/* remove hidden sm:block for the mobile responsive */}
-            <span className=" text-sm !cursor-pointer">{navItem.name}</span>
-          </Link>
-        ))}
-        {/* remove this login btn */}
-        {/* <button className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full">
-          <span>Login</span>
-          <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
-        </button> */}
-      </motion.div>
-    </AnimatePresence>
+    <div onMouseEnter={() => setActive(item)} className="relative ">
+      <motion.p
+        transition={{ duration: 0.3 }}
+        className="cursor-pointer text-black hover:opacity-[0.9] dark:text-white">
+        {item}
+      </motion.p>
+      {active !== null && (
+        <motion.div
+          initial={{ opacity: 0, scale: 0.85, y: 10 }}
+          animate={{ opacity: 1, scale: 1, y: 0 }}
+          transition={transition}>
+          {active === item && (
+            <div
+              className="absolute top-[calc(100%_+_1.2rem)] left-1/2 transform -translate-x-1/2 pt-4">
+              <motion.div
+                transition={transition}
+                // layoutId ensures smooth animation
+                layoutId="active"
+                className="bg-white dark:bg-black backdrop-blur-sm rounded-2xl overflow-hidden border border-black/[0.2] dark:border-white/[0.2] shadow-xl">
+                <motion.div
+                  // layout ensures smooth animation
+                  layout
+                  className="w-max h-full p-4">
+                  {children}
+                </motion.div>
+              </motion.div>
+            </div>
+          )}
+        </motion.div>
+      )}
+    </div>
+  );
+};
+
+export const Menu = ({
+  setActive,
+  children
+}) => {
+  return (
+    <nav
+      // resets the state
+      onMouseLeave={() => setActive(null)}
+      className="relative rounded-full border border-transparent dark:bg-black dark:border-white/[0.2] bg-white shadow-input flex justify-center space-x-4 px-8 py-6 ">
+      {children}
+    </nav>
+  );
+};
+
+export const ProductItem = ({
+  title,
+  description,
+  href,
+  src
+}) => {
+  return (
+    <a href={href} className="flex space-x-2">
+      <img
+        src={src}
+        width={140}
+        height={70}
+        alt={title}
+        className="shrink-0 rounded-md shadow-2xl" />
+      <div>
+        <h4 className="text-xl font-bold mb-1 text-black dark:text-white">
+          {title}
+        </h4>
+        <p className="text-neutral-700 text-sm max-w-[10rem] dark:text-neutral-300">
+          {description}
+        </p>
+      </div>
+    </a>
+  );
+};
+
+export const HoveredLink = ({
+  children,
+  ...rest
+}) => {
+  return (
+    <a
+      {...rest}
+      className="text-neutral-700 dark:text-neutral-200 hover:text-black ">
+      {children}
+    </a>
   );
 };
